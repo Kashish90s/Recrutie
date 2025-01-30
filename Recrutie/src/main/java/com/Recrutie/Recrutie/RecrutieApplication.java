61d67edd495d8c76565a97cff51e12514ecb3ce9
ItsKashishMaharjan <np03cs4a210073@heraldcollege.edu.np>
2025-01-30 16:26:40 +0545

    modular changes and added MVC folders

--- Recrutie/src/main/java/com/Recrutie/Recrutie/RecrutieApplication.java
+++ Recrutie/src/main/java/com/Recrutie/Recrutie/RecrutieApplication.java
@@ -1,4 +1,4 @@
-package Recrutie.Recrutie;
+package com.Recrutie.Recrutie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Recrutie/src/test/java/com/Recrutie/Recrutie/RecrutieApplicationTests.java
+++ Recrutie/src/test/java/com/Recrutie/Recrutie/RecrutieApplicationTests.java
@@ -1,4 +1,4 @@
-package Recrutie.Recrutie;
+package com.Recrutie.Recrutie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
